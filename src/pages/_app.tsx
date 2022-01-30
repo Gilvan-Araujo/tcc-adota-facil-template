64b27ec39b562433520f0f7cfab08afce0110b8f
Gilvan Araújo <gilvanjunior@protonmail.com>
2022-01-30 20:08:28 -0300

    chore: rename global style file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
 
-import GlobalStyle from '../styles/globalStyles'
+import GlobalStyle from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
