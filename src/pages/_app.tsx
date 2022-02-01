f0e9f2e75701a4bb83f05d3f1ee7f371dbf8f3c0
Gilvan Araújo <gilvanjunior@protonmail.com>
2022-02-01 08:04:23 -0300

    chore: improve styles within server-side rendering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,28 @@
+import { CssBaseline, ThemeProvider } from '@material-ui/core'
 import type { AppProps } from 'next/app'
 import React from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 import GlobalStyle from '../styles/global'
+import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles !== null) {
+      if (jssStyles.parentElement !== null)
+        jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
       <ToastContainer position="bottom-center" theme="dark" />
     </>
   )
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import blue from '@material-ui/core/colors/blue'
+import { createTheme } from '@material-ui/core/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: blue
+  }
+})
+
+export default theme
