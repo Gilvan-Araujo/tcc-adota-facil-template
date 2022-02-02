04159b14ef7d7ee67f091e038001536f36bd93d4
Gilvan Araújo <gilvanjunior@protonmail.com>
2022-02-02 20:26:38 -0300

    test: load component tests

--- src/components/Load/styles.ts
+++ src/components/Load/styles.ts
@@ -1,10 +1,12 @@
 import { Backdrop, CircularProgress } from '@material-ui/core'
 import styled from 'styled-components'
 
+import theme from '../../styles/theme'
+
 export const CustomBackdrop = styled(Backdrop)`
   z-index: 1000 !important;
 
-  color: #fff;
+  color: ${theme.palette.common.white};
 `
 
 export const Loader = styled(CircularProgress)``
