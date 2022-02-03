5b008379f4a42621945a8d01dbed627dee18873b
Gilvan Araújo <gilvanjunior@protonmail.com>
2022-02-02 21:46:26 -0300

    refactor: remove unecessary button component

--- src/components/Button/styles.ts
+++
@@ -1,6 +0,0 @@
-import { Button as MUIButton } from '@material-ui/core'
-import styled from 'styled-components'
-
-export const Button = styled(MUIButton)``
-
-export const OutlineButton = styled(MUIButton)``
