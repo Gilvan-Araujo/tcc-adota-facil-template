61a1487ad54f2abde13935736f950a66ecb59e06
Gilvan Araújo <gilvanjunior@protonmail.com>
2022-02-03 19:00:17 -0300

    chore: rename styles file to match new page name

--- src/pages/cadastrarPet/index.tsx
+++ src/pages/cadastrarPet/index.tsx
@@ -9,7 +9,7 @@ import * as Yup from 'yup'
 import Load from '../../components/Load'
 import Images from '../../services/Images'
 import Pets from '../../services/Pets'
-import * as S from '../../styles/pages/Form'
+import * as S from '../../styles/pages/cadastrarPet'
 import createWhatsappLink from '../../utils/createWhatsappLink'
 
 const Form = () => {
