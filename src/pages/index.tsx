9e9a8e6b0fc09e66fccbdbd1f05c53c4acbe9e02
Gilvan Araújo <gilvanjunior@protonmail.com>
2022-02-06 14:53:11 -0300

    refactor: update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,13 @@
-import { Button, Grid } from '@material-ui/core'
+import { Button } from '@material-ui/core'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
-
-import Pets from '@services/Pets'
-
-import Load from '@components/Load'
+import React from 'react'
 
 import HomeContainer from '@styles/pages/Home'
 
 function Home() {
-  const [data, setData] = useState<any[]>([])
-  const [loading, setLoading] = useState(true)
-
-  const getData = async () => {
-    setLoading(true)
-
-    try {
-      Pets.getPets().then((res) => {
-        const tmpData = res.data.map((item: any) => item.data)
-
-        setData(tmpData)
-        setLoading(false)
-      })
-    } catch (error) {
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    getData()
-  }, [])
-
   return (
-    <HomeContainer>
-      <Load loading={loading} />
+    <>
       <Head>
         <title>Adota Fácil</title>
         <meta
@@ -46,30 +17,20 @@ function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Home page</h1>
-      <Grid container spacing={3}>
-        {data.map((item: any) => (
-          <Grid item xs={12} sm={6} md={4} key={uuidv4()}>
-            {item.image && (
-              <Image
-                src={item.image}
-                alt={item.name}
-                width={300}
-                height={300}
-                objectFit="cover"
-              />
-            )}
-            <span>
-              {item.name} - {item.type}
-            </span>
-          </Grid>
-        ))}
-      </Grid>
-      <Link href="/cadastrarPet" passHref>
-        <Button variant="contained" color="primary">
-          Ir para o form
-        </Button>
-      </Link>
-    </HomeContainer>
+      <HomeContainer>
+        <Link href="/pets" passHref>
+          <Button variant="contained" color="primary">
+            Ir para a listagem
+          </Button>
+        </Link>
+
+        <Link href="/cadastrarPet" passHref>
+          <Button variant="contained" color="primary">
+            Ir para o form
+          </Button>
+        </Link>
+      </HomeContainer>
+    </>
   )
 }
 
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -2,8 +2,11 @@ import styled from 'styled-components'
 
 const HomeContainer = styled.div`
   display: flex;
+  height: 100px;
+  width: calc(100vw - 4rem);
   padding: 2rem;
   margin: 2rem;
+  justify-content: space-between;
 `
 
 export default HomeContainer
